6e744dc269eb01cf1215f1abebb606842c02571d
Shafreen123 <shaziya0205@gmail.com>
2026-05-07 11:24:17 +0530

    Adding DTOs Validation

--- BusTicketSystem.Web/DTOs/BookingRequestDto.cs
+++ BusTicketSystem.Web/DTOs/BookingRequestDto.cs
@@ -2,6 +2,7 @@ namespace BusTicketSystem.Web.DTOs;
 
 public class BookingRequestDto
 {
+    
     public int TripId { get; set; }
     public int SeatNumber { get; set; }
     public int CustomerId { get; set; }
--- BusTicketSystem.Web/Program.cs
+++ BusTicketSystem.Web/Program.cs
@@ -1,4 +1,7 @@
 using BusTicketSystem.Web.Models;
+using BusTicketSystem.Web.Validator;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +11,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database
 builder.Services.AddDbContext<BusTicketDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// FluentValidation
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<BookingValidator>();
 
 var app = builder.Build();
 
@@ -21,9 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
