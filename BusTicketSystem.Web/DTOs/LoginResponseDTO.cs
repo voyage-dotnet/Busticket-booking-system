add375eb4b3ba56cade916948f750f9bce2ce66b
Hritik singh <hritiksingh158@gmail.com>
2026-05-10 15:05:22 +0530

    Merge conflict resolved between auth and agency branch

---
+++ BusTicketSystem.Web/DTOs/LoginResponseDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusTicketSystem.Web.DTOs
+{
+    public class LoginResponseDTO
+    {
+        public string? Token {get; set;}
+    }
+}
---
+++ BusTicketSystem.Web/DTOs/RegisterRequestDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusTicketSystem.Web.DTOs
+{
+    public class RegisterRequestDTO
+    {
+        public string Name { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Phone { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
